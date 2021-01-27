d088659f3d7d389b8f5610c7823a06101162b06f
Sophie <spavia@me.com>
2021-01-27 12:13:46 -0500

    Init. Hello World Spring Boot App

---
+++ src/main/java/com/example/HelloWorld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "HelloWorld";
+    }
+
+}
